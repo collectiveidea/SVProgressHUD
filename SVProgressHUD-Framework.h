9f8ace0ce9f0290a8316b2dcaa52004e6a122e5c
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2015-12-04 11:47:34 -0500

    Fix warning when including SVProgressHUD as an embedded Framework:
    When installing SVProgressHUD with Carthage, I received the following warning:
    ```
    <module-includes>:1:1: Umbrella header for module 'SVProgressHUD' does not include header 'SVProgressHUD-Framework.h'
    ```
    
    By including an empty header, this should quiet the warning.

---
+++ SVProgressHUD-Framework.h
@@ -0,0 +1,15 @@
+//
+//  SVProgressHUD-Framework.h
+//  SVProgressHUD
+//
+//  Created by Chris Rittersdorf on 12/4/15.
+//  Copyright © 2015 EmbeddedSources. All rights reserved.
+//
+
+
+// This is an empty header file to quiet warnings for
+#ifndef SVProgressHUD_Framework_h
+#define SVProgressHUD_Framework_h
+
+
+#endif /* SVProgressHUD_Framework_h */
